9e957ac7a4dba2dd3d7f16f7fc1a2f4ee7bdf479
SoyLaCerda <martine.miguel.14@gmail.com>
2024-07-24 21:14:48 +0000

    Mundo guardado 2024-07-24

--- server.py
+++ server.py
@@ -15,11 +15,10 @@
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
-# us-cal-1	      Estados unidos (California)
+# us-cal-1	      Estados unidos (California
 
 
 
-3
 
 # No toques nada de aquí para abajo, puedes dañarlo
 import requests,os,base64,glob,time
